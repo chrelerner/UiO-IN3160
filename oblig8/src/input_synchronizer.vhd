d355f12d0fce41e1166f8f719445cec9b3f57171
chrisrle <chrisrle@uio.no>
2023-04-09 22:24:05 +0200

    Included File IO in self_test_module.vhd. Updated input_synchronizer.vhd to use single bits instead of vectors.

--- oblig8/src/input_synchronizer.vhd
+++ oblig8/src/input_synchronizer.vhd
@@ -2,29 +2,26 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity input_synchronizer is
-  generic (
-            bit_amount : natural := 10;
-            );
   port (
          mclk     : in std_logic;
          reset    : in std_logic;
-         SA    : in std_logic_vector(bit_amount-1 downto 0);
-         SB    : in std_logic_vector(bit_amount-1 downto 0);
-         SA_synch : out std_logic_vector(bit_amount-1 downto 0);
-         SB_synch : out std_logic_vector(bit_amount-1 downto 0)
+         SA       : in std_logic;
+         SB       : in std_logic;
+         SA_synch : out std_logic;
+         SB_synch : out std_logic
          );
 end input_synchronizer;
 
 architecture rtl of input_synchronizer is
-  signal SA_middle, SB_middle : std_logic_vector(bit_amount-1 downto 0);
+  signal SA_middle, SB_middle : std_logic;
 begin
 
   BRUTE_FORCE:
   process (mclk, reset) is
   begin
     if (reset = '1') then
-      SA_synch <= (others => '0');
-      SB_synch <= (others => '0');
+      SA_synch <= '0';
+      SB_synch <= '0';
     elsif rising_edge(mclk) then
       SA_middle <= SA;
       SA_synch <= SA_middle;
--- oblig8/src/self_test_module.vhd
+++ oblig8/src/self_test_module.vhd
@@ -1,16 +1,20 @@
+-- This file uses code taken from lecture slides on File IO.
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use STD.textio.all;
 
 entity self_test_module is
   generic (
             addr_width : natural := 5; -- 32 data instances require 5 bit addresses,
-            data_width : natural := 8; -- 8 bit data. 
+            data_width : natural := 8; -- 8 bit data.
+            filename : string := "duty_cycles_file.txt"
             );
   port (
          mclk        : in std_logic;
          reset       : in std_logic;
-	 duty_cycle : out std_logic_vector(7 downto 0);
+	 duty_cycle  : out std_logic_vector(7 downto 0);
          );
 end self_test_module;
 
@@ -19,13 +23,19 @@ architecture rtl of self_test_module is
   type memory_array is array(0 to (2**addr_width)-1) of
     std_logic_vector(data_width-1 downto 0);
 
-  constant ROM_DATA : memory array :=
-  (
+  impure function intitialize_ROM(file_name: string);
+    return memory_array is
+    file init_file        : text open read mode is filename;
+    variable current_line : line;
+    variable result       : memory_array;
+  begin
+    for i in result'range loop
+      readline(init_file, current_line)
+      read(current_line, result(i));
+    end loop;
+    return result:
 
-    -- Fill in the data with 31 entries, last slot has to be b"0".
-    -- The values are meant to be changed, therefore use FILE IO.
-
-  );
+  constant ROM_DATA : memory array := initialize_ROM(filename);
 
   signal second_tick : std_logic;
   signal counter     : unsigned (28 downto 0) := (others => '0');
@@ -43,9 +53,9 @@ begin
       counter <= (others => '0');
       second_tick <= '0';
     elsif rising_Edge(mclk) then
-      increment := (others => '0') when (counter = b"299999999") else counter + '1';
+      increment := (others => '0') when (counter = d"299999999") else counter + '1';
       counter <= increment;
-      second_tick <= '1' when (increment = b"299999999") else '0';
+      second_tick <= '1' when (increment = d"299999999") else '0';
     end if;
   end process SECOND_TICK_GENERATOR;
 
@@ -55,17 +65,17 @@ begin
   begin
     if (reset = '1') then
       address <= (others => '0');
-      data_out <= (others => '0');
+      data <= (others => '0');
     elsif rising_Edge(mclk) then
       increment := unsigned(address);
       address <= std_logic(increment) when ((second_tick = '1') and (increment = b"31")) else
                  std_logic_vector(increment + '1') when (second_tick = '1') else
                  std_logic_Vector(increment);
+      data <= ROM_DATA(to_integer(unsigned(address)));
     end if;
   end process UPDATING_SIGNAL;
 
-  data <= ROM_DATA(to_integer(unsigned(address)));
-  duty_cycle <= data_out;
+  duty_cycle <= data;
 
 end architecture rtl;
 
